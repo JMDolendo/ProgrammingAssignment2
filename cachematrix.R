0b99d7c0c4000f4ed88d7ca4ad0b7438728c6d32
JMDolendo <jmdolendo18@gmail.com>
2017-12-02 17:33:15 +0800

    Programming Assignment 2 - Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Peer-graded Assignment: Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+
+## makeCacheMatrix is a function that creates a special matrix "object" (x).
+## It returns a list of functions that stores and get the given matrix and its
+## inverse. The list contains:
+## [1] Set the value of the matrix (set_func)
+## [2] Get the value of the matrix (get_func)
+## [3] Set the value of the inverse (set_inv)
+## [4] Get the value of the inverse (get_inv)
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   i <- NULL
+  set_func <- function(y){
+      x<<-y
+      i<<-NULL
+  }
+  get_func <- function() x
+  set_inv <- function(inverse) i <<- inverse
+  get_inv <- function() i
+  list(set_func=set_func,get_func=get_func, set_inv= set_inv, get_inv=get_inv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function computes the inverse of the special "matrix" returned
+## by makeCacheMatrix above. Initially, it will check if the inverse has already
+## been calculated, this is done using the "if" condition which check if there's
+## a value already stored in get_inverse. If there has already values stored, it 
+## will skip the computation and retrieve the stored data. Otherwise, it will compute 
+## the inverse of the matrix using "solve" function. This will be stored in
+## variable "i" and will be stored in makecacheMatrix set_inv as "i" in different environment.
+## Finally, the inverse matrix is returned using "i".
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$get_inv()
+        if(!is.null(i)){
+          message("getting cached data")
+          return(i)
+        }
+        dat <-x$get_func()
+        i <- solve(dat)
+        x$set_inv(i)
+        i
 }
